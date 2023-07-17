3e2aa46c6e0f2daa6ab74508cc07bde571254963
skyfalling <skyfalling@live.com>
2023-07-17 21:43:36 +0800

    repair project config

--- src/main/java/leetcode/editor/cn/BuildingH2o.java
+++ src/main/java/leetcode/editor/cn/BuildingH2o.java
@@ -49,6 +49,8 @@
 
 package leetcode.editor.cn;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
@@ -57,7 +59,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class BuildingH2o {
     public static void main(String[] args) throws InterruptedException {
-        H2O h2O = new H2O();
+        H2O h2O = new H2O(new LH2O());
         for (int i = 0; i < 20; i++) {
             new Thread(() -> {
                 try {
@@ -82,31 +84,84 @@ public class BuildingH2o {
 }
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
+interface IH2O {
+    void hydrogen(Runnable releaseHydrogen) throws InterruptedException;
+
+    void oxygen(Runnable releaseOxygen) throws InterruptedException;
+
+    default boolean genWater(AtomicInteger hNum, AtomicInteger oNum) {
+        if (hNum.get() == 2 && oNum.get() == 1) {
+            hNum.set(0);
+            oNum.set(0);
+            System.out.println();
+            return true;
+        }
+        return false;
+    }
+}
+
+abstract class AH2O implements IH2O {
+
+    protected final AtomicInteger hNum = new AtomicInteger();
+    protected final AtomicInteger oNum = new AtomicInteger();
+
+    protected boolean genWater() {
+        if (hNum.get() == 2 && oNum.get() == 1) {
+            hNum.set(0);
+            oNum.set(0);
+//            System.out.println();
+            return true;
+        }
+        return false;
+    }
+}
+
+
 class H2O {
+
+    private IH2O ih2O;
+
+    public H2O(IH2O ih2O) {
+        this.ih2O = ih2O;
+    }
+
+    public H2O() {
+        this(new CH2O());
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        ih2O.hydrogen(releaseHydrogen);
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        ih2O.oxygen(releaseOxygen);
+    }
+
+
+}
+
+class LH2O extends AH2O {
 
     private Lock lock = new ReentrantLock();
     //h原子满足条件
     private Condition hReady = lock.newCondition();
     //o原子满足条件
     private Condition oReady = lock.newCondition();
-    private volatile int hNum = 0;
-    private volatile int oNum = 0;
 
     public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
         lock.lock();
         try {
-            while (hNum == 2) {//H原子已就绪，此时要么合成水分子，要么等待生产氧原子
+            while (hNum.get() == 2) {//H原子已就绪，此时要么合成水分子，要么等待生产氧原子
                 if (!genWater()) {//是否可以合成水分子
                     hReady.await();
                 }
             }
             // releaseHydrogen.run() outputs "H". Do not change or remove this line.
             releaseHydrogen.run();
-            hNum++;
+            hNum.incrementAndGet();
             hReady.signalAll();
             oReady.signalAll();
-        } catch (Exception e) {
-            e.printStackTrace();
         } finally {
             lock.unlock();
         }
@@ -115,36 +170,24 @@ class H2O {
     public void oxygen(Runnable releaseOxygen) throws InterruptedException {
         lock.lock();
         try {
-            while (oNum == 1) {//O原子已就绪，此时要么合成水分子，要么等待H原子
+            while (oNum.get() == 1) {//O原子已就绪，此时要么合成水分子，要么等待H原子
                 if (!genWater()) {//是否可以合成水分子
                     oReady.await();
                 }
             }
             // releaseOxygen.run() outputs "O". Do not change or remove this line.
             releaseOxygen.run();
-            oNum++;
+            oNum.incrementAndGet();
             hReady.signalAll();
             oReady.signalAll();
-        } catch (Exception e) {
-            e.printStackTrace();
         } finally {
             lock.unlock();
         }
     }
-
-    private boolean genWater() {
-        if (hNum == 2 && oNum == 1) {
-            hNum -= 2;
-            oNum--;
-            System.out.println();
-            return true;
-        }
-        return false;
-    }
-
 }
 
-class H2O3 {
+
+class SH2O extends AH2O {
 
     //实际上只需两个信号量的和为2。
     // 如果Semaphore_h=2，那么就是先释放两个H，然后释放一个O，输出结果：HHO；
@@ -156,41 +199,11 @@ class H2O3 {
 
     public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
         h.acquire();
-        releaseHydrogen.run();
-        o.release();
-    }
-
-    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
-        o.acquire(2);
-        // releaseOxygen.run() outputs "O". Do not change or remove this line.
-        releaseOxygen.run();
-        h.release(2);
-    }
-}
-
-class H2O2 {
-
-    //实际上只需两个信号量的和为2。
-    // 如果Semaphore_h=2，那么就是先释放两个H，然后释放一个O，输出结果：HHO；
-    // 如果是Semaphore_o=2，那么就是先释放一个O，然后释放两个H，输出结果：OHH；
-    // 如果Semaphore_h=1，Semaphore_o=1，那就是先释放一个H，再释放一个O，再释放一个H，输出结果：HOH；
-    private final AtomicInteger hNum = new AtomicInteger(0);
-    private final AtomicInteger oNum = new AtomicInteger(0);
-    private Semaphore h = new Semaphore(1);
-    private Semaphore o = new Semaphore(1);
-
-
-    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-        h.acquire();
-        synchronized (hNum) {
+        synchronized (this) {
             // releaseHydrogen.run() outputs "H". Do not change or remove this line.
             releaseHydrogen.run();
             hNum.incrementAndGet();
-            if (hNum.get() == 2 && oNum.get() == 1) {//H原子就绪，直接合成水分子
-                hNum.addAndGet(-2);
-                oNum.addAndGet(-1);
-                System.out.println();
-            }
+            genWater();
         }
 
         o.release();
@@ -201,13 +214,45 @@ class H2O2 {
         // releaseOxygen.run() outputs "O". Do not change or remove this line.
         releaseOxygen.run();
         oNum.incrementAndGet();
-        if (hNum.get() == 2 && oNum.get() == 1) {//H原子就绪，直接合成水分子
-            hNum.addAndGet(-2);
-            oNum.addAndGet(-1);
-            System.out.println();
-        }
+        genWater();
         h.release(2);
     }
+
 }
+
+class CH2O extends AH2O {
+    private Semaphore h = new Semaphore(2);
+    private Semaphore o = new Semaphore(1);
+    private CyclicBarrier cb = new CyclicBarrier(3);
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        h.acquire();
+        try {
+            cb.await();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        releaseHydrogen.run();
+        hNum.incrementAndGet();
+        genWater();
+        h.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        o.acquire();
+        try {
+            cb.await();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        releaseOxygen.run();
+        oNum.incrementAndGet();
+        genWater();
+        o.release();
+    }
+
+
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
 
