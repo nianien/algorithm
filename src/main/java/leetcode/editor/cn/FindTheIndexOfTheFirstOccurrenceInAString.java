8b510cdcbee2df8efc41d92268acbffb8f47e0a3
skyfalling <skyfalling@live.com>
2023-07-20 17:41:20 +0800

    reload

--- src/main/java/leetcode/editor/cn/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/main/java/leetcode/editor/cn/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,38 +1,24 @@
-//28.implement-strstr
-//实现 strStr() 函数。 
-//
-// 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如
-//果不存在，则返回 -1 。 
-//
-// 
-//
-// 说明： 
-//
-// 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。 
-//
-// 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。 
+//28.find-the-index-of-the-first-occurrence-in-a-string
+//给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。
+//如果 needle 不是 haystack 的一部分，则返回 -1 。 
 //
 // 
 //
 // 示例 1： 
 //
 // 
-//输入：haystack = "hello", needle = "ll"
-//输出：2
+//输入：haystack = "sadbutsad", needle = "sad"
+//输出：0
+//解释："sad" 在下标 0 和 6 处匹配。
+//第一个匹配项的下标是 0 ，所以返回 0 。
 // 
 //
 // 示例 2： 
 //
 // 
-//输入：haystack = "aaaaa", needle = "bba"
+//输入：haystack = "leetcode", needle = "leeto"
 //输出：-1
-// 
-//
-// 示例 3： 
-//
-// 
-//输入：haystack = "", needle = ""
-//输出：0
+//解释："leeto" 没有在 "leetcode" 中出现，所以返回 -1 。
 // 
 //
 // 
@@ -40,18 +26,18 @@
 // 提示： 
 //
 // 
-// 0 <= haystack.length, needle.length <= 5 * 104 
+// 1 <= haystack.length, needle.length <= 10⁴ 
 // haystack 和 needle 仅由小写英文字符组成 
 // 
-// Related Topics 双指针 字符串 
-// 👍 885 👎 0
+//
+// Related Topics 双指针 字符串 字符串匹配 👍 1914 👎 0
 
 
 package leetcode.editor.cn;
 
-public class ImplementStrstr {
+public class FindTheIndexOfTheFirstOccurrenceInAString {
     public static void main(String[] args) {
-        Solution solution = new ImplementStrstr().new Solution();
+        Solution solution = new FindTheIndexOfTheFirstOccurrenceInAString().new Solution();
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
