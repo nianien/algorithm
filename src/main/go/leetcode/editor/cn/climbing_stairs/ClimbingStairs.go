e19364c5645e74b6b1ef90afe14e47dcd7cafb31
skyfalling <skyfalling@live.com>
2023-07-28 15:26:02 +0800

    解决golang一个目录下方法冲突的问题

---
+++ src/main/go/leetcode/editor/cn/climbing_stairs/ClimbingStairs.go
@@ -0,0 +1,78 @@
+//70.climbing-stairs
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：n = 2
+//输出：2
+//解释：有两种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶
+//2. 2 阶
+//
+// 示例 2：
+//
+//
+//输入：n = 3
+//输出：3
+//解释：有三种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶 + 1 阶
+//2. 1 阶 + 2 阶
+//3. 2 阶 + 1 阶
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= n <= 45
+//
+//
+// Related Topics 记忆化搜索 数学 动态规划 👍 3139 👎 0
+
+package main
+
+import (
+	"fmt"
+)
+
+// leetcode submit region begin(Prohibit modification and deletion)
+
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	dp := make([]int, n)
+	dp[0] = 1
+	dp[1] = 2
+	for i := 2; i < n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n-1]
+}
+
+func climbStairs2(n int) int {
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	return climbStairs(n-1) + climbStairs(n-2)
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+// test from here
+func main() {
+	fmt.Println(climbStairs(44))
+	fmt.Println(climbStairs2(44))
+}
